b224add80590e4e6abb50174f0223cf8509fe6d9
dubisdev <dubisdev@gmail.com>
2023-01-12 14:30:40 +0100

    feat: `alt+q` quits the app

--- src/utils/configureShortcuts.ts
+++ src/utils/configureShortcuts.ts
@@ -1,17 +1,24 @@
 import { register, unregisterAll } from "@tauri-apps/api/globalShortcut";
+import { exit } from "@tauri-apps/api/process";
 import { toggleWindowVisibility } from "./toggleWindowView";
 
 export const configureShortcuts = () => {
   unregisterAll();
-  // register a global shortcut
-  register("Alt+m", () => {
-    toggleWindowVisibility();
-  });
 
-  // register a shortcut but only when the window is focused
-  window.addEventListener("keydown" as any, (e: KeyboardEvent) => {
-    if (e.key === "Enter") {
+  // register global shortcuts (works even when the app is not focused)
+  register("Alt+m", toggleWindowVisibility);
+
+  // register shortcuts when the window is focused
+  window.addEventListener("keydown", async (e) => {
+    if (isExitCommand(e)) {
+      await exit();
+    }
+
+    if (isCopyCommand(e)) {
       window.dispatchEvent(new Event("copy-to-clipboard"));
     }
   });
 };
+
+const isExitCommand = (e: KeyboardEvent) => e.key === "q" && e.altKey;
+const isCopyCommand = (e: KeyboardEvent) => e.key === "Enter";
