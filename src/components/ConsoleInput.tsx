4f5e8d03334b409d7752a3ee4d40d1be42da009c
dubisdev <dubisdev@gmail.com>
2023-01-12 16:47:10 +0100

    chore: improve placeholder

--- src/components/ConsoleInput.tsx
+++ src/components/ConsoleInput.tsx
@@ -1,15 +1,16 @@
 import { useCalculatorStore } from "../state/calculator";
 import styles from "./console.module.css";
 
+const dateAsSum = new Date().toLocaleDateString().split("/").join("+");
+
 export const ConsoleInput = () => {
   const [input, setInput] = useCalculatorStore((s) => [s.input, s.setInput]);
 
   return (
     <input
-      type="text"
       autoFocus
       className={styles.consoleInput}
-      placeholder="Do math here!"
+      placeholder={dateAsSum}
       value={input}
       onChange={(e) => setInput(e.target.value)}
     />
