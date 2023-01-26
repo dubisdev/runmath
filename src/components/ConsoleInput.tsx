666b2449afcad8840d824483bb8a27d8fe3a4967
dubisdev <dubisdev@gmail.com>
2023-01-26 11:16:58 +0100

    feat: add placeholder animation

--- src/components/ConsoleInput.tsx
+++ src/components/ConsoleInput.tsx
@@ -1,18 +1,29 @@
 import { useCalculatorStore } from "@state/calculator";
+import Textra from "react-textra";
 import styles from "./console.module.css";
 
 const dateAsSum = new Date().toLocaleDateString().split("/").join("+");
+
+const placeHolderContents = [dateAsSum, "alt + q", "alt + s"];
 
 export const ConsoleInput = () => {
   const [input, setInput] = useCalculatorStore((s) => [s.input, s.setInput]);
 
   return (
-    <input
-      autoFocus
-      className={styles.consoleInput}
-      placeholder={dateAsSum}
-      value={input}
-      onChange={(e) => setInput(e.target.value)}
-    />
+    <>
+      {!input && (
+        <Textra
+          data={placeHolderContents}
+          effect="scale"
+          className={styles.placeholder}
+        />
+      )}
+      <input
+        autoFocus
+        className={styles.consoleInput}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+    </>
   );
 };
