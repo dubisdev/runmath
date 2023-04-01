4fb2f68818e0497bb9efc52c68db49b69c9025d5
dubisdev <dubisdev@gmail.com>
2023-04-02 00:43:11 +0200

    feat: Add tray icon with toggle and quit utilities

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,29 +1,67 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use tauri::{
+    AppHandle, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu,
+    SystemTrayMenuItem,
+};
 use tauri_plugin_autostart::MacosLauncher;
-use tauri::Manager;
 
 #[derive(Clone, serde::Serialize)]
 struct Payload {
-  args: Vec<String>,
-  cwd: String,
+    args: Vec<String>,
+    cwd: String,
 }
 
 fn main() {
-  let context = tauri::generate_context!();
-  tauri::Builder::default()
-    .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, Some(vec![""])))
-    .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
-      app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
-    }))
-    .menu(if cfg!(target_os = "macos") {
-      tauri::Menu::os_default(&context.package_info().name)
+    let tray = create_tray_menu();
+    let context = tauri::generate_context!();
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_autostart::init(
+            MacosLauncher::LaunchAgent,
+            Some(vec![""]),
+        ))
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            app.emit_all("single-instance", Payload { args: argv, cwd })
+                .unwrap();
+        }))
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick { .. } => toggle_window(app),
+
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => std::process::exit(0),
+                "toggle" => toggle_window(app),
+                _ => {}
+            },
+            _ => {}
+        })
+        .run(context)
+        .expect("error while running tauri application");
+}
+
+fn create_tray_menu() -> SystemTray {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let toggle = CustomMenuItem::new("toggle".to_string(), "Toggle App");
+
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(toggle)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+
+    SystemTray::new().with_menu(tray_menu)
+}
+
+fn toggle_window(app: &AppHandle) {
+    let window = app.get_window("main").unwrap();
+    if window.is_visible().unwrap() {
+        window.hide().unwrap();
+        return;
     } else {
-      tauri::Menu::default()
-    })
-    .run(context)
-    .expect("error while running tauri application");
+        window.show().unwrap();
+        return;
+    }
 }
