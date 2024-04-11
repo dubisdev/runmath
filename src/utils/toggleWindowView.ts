4c7530b9d211a0003efdb7912f42b4c37b908e57
dubisdev <dubisdev@gmail.com>
2024-04-11 12:24:09 +0200

    chore: remove old commetn

--- src/utils/toggleWindowView.ts
+++ src/utils/toggleWindowView.ts
@@ -1,6 +1,5 @@
 import { appWindow } from "@tauri-apps/api/window";
 
-// TODO - Wait for https://github.com/tauri-apps/tauri/pull/5815 to use minimize instead of hide
 export const toggleWindowVisibility = async () => {
   if (await appWindow.isVisible()) {
     await appWindow.hide();
