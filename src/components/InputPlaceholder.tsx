288ff9cf3c7f451251af589c26a9b3caef5943ed
dubisdev <dubisdev@gmail.com>
2024-04-24 21:38:58 +0200

    fix: remove old shortcut information

--- src/components/InputPlaceholder.tsx
+++ src/components/InputPlaceholder.tsx
@@ -8,7 +8,6 @@ const placeHolderContents = [
   "alt + q = quit",
   "alt + s = settings",
   "alt + m = toggle visibility",
-  "alt + u = go to download page",
   "enter = copy to clipboard",
   "tab = select all",
 ];
