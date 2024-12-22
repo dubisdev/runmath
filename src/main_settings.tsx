ca4e85e03790ce28822beda8dd0be7559cefa3e5
dubisdev <dubisdev@gmail.com>
2024-12-22 13:54:12 +0100

    chore: Small refactor

--- src/main_settings.tsx
+++ src/main_settings.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Settings } from "@components/Settings";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = createRoot(document.getElementById("root")!)
+root.render(
   <React.StrictMode>
     <Settings />
   </React.StrictMode>
